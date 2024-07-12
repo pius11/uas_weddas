fdc15d1f87603d899498b832d845cb539cc860c0
BaliBlackhat <pius17431@gmail.com>
2024-07-12 16:46:11 +0800

    modif koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","piuss","","pos");
+$koneksi = mysqli_connect("localhost","root","","pos");
 
 if (mysqli_connect_errno()) {
     echo "koneksi error : ". mysqli_connect_error() ;
